f9579e64a9eaf788dc02218e08fece8e44b94f79
Cynthia Pan <cynthiapan11@gmail.com>
2025-09-21 14:01:36 -0700

    get user's location (latitude, longitude) with Geolocation API

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,20 +3,41 @@ import api from '../api/axios';
 import SideBar from '../components/SideBar';
 
 const Home = () => {
+  const [_, setUserLocation] = useState(null);
   const [stops, setStops] = useState([]);
 
   useEffect(() => {
-    const getStops = async () => {
+    const fetchData = async () => {
       try {
-        const response = await api.get('/stops');
-        setStops(response.data);
+        await getStops();
+        getUserLocation();
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
     };
 
-    getStops();
+    fetchData();
   }, []);
+
+  const getStops = async () => {
+    try {
+      const response = await api.get('/stops');
+      setStops(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUserLocation = () => {
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser.');
+    }
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log('User Location:', latitude, longitude);
+      setUserLocation({ latitude, longitude });
+    });
+  };
 
   return (
     <div className="flex h-screen">
