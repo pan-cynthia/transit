8a98127906d7eaf1145a15e9fd203cb797b9bb18
Cynthia Pan <cynthiapan11@gmail.com>
2025-09-23 12:40:09 -0700

    fetch all lines and display them in dropdown menu on search page

--- frontend/src/components/DropDown.jsx
+++ frontend/src/components/DropDown.jsx
@@ -1,7 +1,12 @@
-const DropDown = () => {
+const DropDown = ({ options }) => {
   return (
     <select>
       <option value="">Select a Route</option> {/* Default */}
+      {options.map((option) => (
+        <option key={option.id} value={option.id}>
+          {option.id} {option.name}
+        </option>
+      ))}
     </select>
   );
 };
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
+import api from '../api/axios';
 import DropDown from '../components/DropDown';
 
 const Search = () => {
+  const [lines, setLines] = useState([]);
+
+  useEffect(() => {
+    const getLines = async () => {
+      try {
+        const response = await api.get('/lines');
+        setLines(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getLines();
+  }, []);
+
   return (
     <div className="mx-auto flex h-screen w-3/5 flex-col justify-center">
       <div className="flex h-[80vh] flex-col items-center rounded-xl border-2 border-slate-600 bg-amber-100">
         <h2 className="mt-7 p-5 text-xl font-medium">Find Stop by Route</h2>
         <div className="mt-7 flex w-3/4 justify-between rounded-xl bg-amber-200 p-5">
           <label className="font-medium">Route</label>
-          <DropDown />
+          <DropDown options={lines} />
         </div>
       </div>
     </div>
