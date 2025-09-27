855220befd8911adbfb8be4bbb2c02bf8f80bc25
Cynthia Pan <cynthiapan11@gmail.com>
2025-09-26 20:56:18 -0700

    setup postgresql db

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import pkg from "pg";
+const { Pool } = pkg;
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT
+});
+
+export default pool;
