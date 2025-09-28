75884180d06be555113a3051d5c7e399d89852c3
Cynthia Pan <cynthiapan11@gmail.com>
2025-09-28 12:51:37 -0700

    update /routes query to order routes by route ids

--- backend/queries.js
+++ backend/queries.js
@@ -7,6 +7,9 @@ router.get("/routes", async (req, res) => {
   try {
     const query = `
       SELECT route_id, route_short_name, route_long_name FROM routes
+      ORDER BY CASE WHEN route_id ~ '^[A-Za-z]+$' THEN 0 ELSE 1 END,
+      CASE WHEN route_id ~ '^[0-9]+' THEN CAST(regexp_replace(route_id, '[^0-9].*$', '', 'g') AS integer) ELSE NULL END,
+      route_id
     `;
     const response = await pool.query(query);
     res.json(response.rows);
