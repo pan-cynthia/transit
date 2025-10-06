ea1ae29df02bdf0921760c47b69a0bb6c770e609
Cynthia Pan <cynthiapan11@gmail.com>
2025-10-06 13:17:51 -0700

    change environment variable names

--- backend/db.js
+++ backend/db.js
@@ -5,11 +5,11 @@ const { Pool } = pkg;
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT
 });
 
 export default pool;
