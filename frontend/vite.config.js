af37a10329d4bd2c81039b1ff3d32b9a9cc7d2ea
Cynthia Pan <cynthiapan11@gmail.com>
2025-10-07 20:03:27 -0700

    add env variable to specify whether app is running on docker or local

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://backend:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.dirname, '');
+  const isDocker = env.NODE_ENV === 'docker';
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true,
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: isDocker ? 'http://backend:3000' : 'http://localhost:3000',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
