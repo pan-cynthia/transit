78f9bdfdbee3d92df74ee30cb140a5f272776bfa
Cynthia Pan <cynthiapan11@gmail.com>
2025-10-23 22:02:29 -0700

    only capitalize first letter of each word in route name

--- backend/queries.js
+++ backend/queries.js
@@ -49,10 +49,25 @@ router.get("/nearby", async (req, res) => {
 router.get("/routes", async (req, res) => {
   try {
     const query = `
-      SELECT route_id, route_short_name, route_long_name FROM routes
-      ORDER BY CASE WHEN route_id ~ '^[A-Za-z]+$' THEN 0 ELSE 1 END,
-      CASE WHEN route_id ~ '^[0-9]+' THEN CAST(regexp_replace(route_id, '[^0-9].*$', '', 'g') AS integer) ELSE NULL END,
-      route_id
+      SELECT
+        route_id,
+        CASE
+          WHEN route_short_name ~ '^[A-Za-z]+$'
+            THEN INITCAP(route_short_name)
+          ELSE route_short_name
+        END AS route_short_name, 
+        INITCAP(route_long_name) AS route_long_name
+      FROM routes
+      ORDER BY
+        CASE
+          WHEN route_id ~ '^[A-Za-z]+$' THEN 0
+          ELSE 1
+        END,
+        CASE
+          WHEN route_id ~ '^[0-9]+' THEN CAST(regexp_replace(route_id, '[^0-9].*$', '', 'g') AS integer)
+          ELSE NULL
+        END,
+        route_id;
     `;
     const response = await pool.query(query);
     res.json(response.rows);
