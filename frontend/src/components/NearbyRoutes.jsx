eacc73503fbd4a4eb914b9070b032d6a7e3fa004
Cynthia Pan <cynthiapan11@gmail.com>
2025-10-25 21:07:28 -0700

    if no nearby stops, display message on screen

--- frontend/src/components/NearbyRoutes.jsx
+++ frontend/src/components/NearbyRoutes.jsx
@@ -3,9 +3,15 @@ import RouteCard from './RouteCard';
 const NearbyRoutes = ({ nearbyStops }) => {
   return (
     <div className="w-2/5 overflow-y-scroll p-5">
-      {nearbyStops.map((stop) => {
-        return <RouteCard key={stop.stop_id} stop={stop} />;
-      })}
+      {Object.keys(nearbyStops).length > 0 ? (
+        <>
+          {nearbyStops.map((stop) => {
+            return <RouteCard key={stop.stop_id} stop={stop} />;
+          })}
+        </>
+      ) : (
+        <div>No Nearby Stops</div>
+      )}
     </div>
   );
 };
