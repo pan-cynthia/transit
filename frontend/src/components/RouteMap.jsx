e7c021db8f2720b391f73f24fa147cc3316e260e
Cynthia Pan <cynthiapan11@gmail.com>
2025-10-28 14:19:18 -0700

    make selected stop icon red instead of blue

--- frontend/src/components/RouteMap.jsx
+++ frontend/src/components/RouteMap.jsx
@@ -1,3 +1,4 @@
+import L from 'leaflet';
 import { useEffect, useState } from 'react';
 import {
   MapContainer,
@@ -34,6 +35,17 @@ const RouteMap = ({ stop, direction, vehicleLocations }) => {
     parseFloat(s.shape_pt_lon),
   ]);
 
+  const stopIcon = new L.icon({
+    iconUrl:
+      'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    shadowUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
   return (
     <>
       <MapContainer
@@ -45,7 +57,7 @@ const RouteMap = ({ stop, direction, vehicleLocations }) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={[latitude, longitude]}>
+        <Marker position={[latitude, longitude]} icon={stopIcon}>
           <Popup>{stop.stop_name}</Popup>
         </Marker>
         <Polyline pathOptions={{ color: 'red' }} positions={polyline} />
