896576bbffdb7a1d9d934b42ab2c72b4ae7e993d
Cynthia Pan <cynthiapan11@gmail.com>
2025-11-03 12:49:46 -0800

    Updates

---
+++ frontend/.prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+};
